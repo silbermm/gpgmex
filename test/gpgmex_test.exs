0f376bf8d1375df8a1f4c2ff570254287afd3ba7
Matt Silbernagel <matt@silbernagel.dev>
2021-12-09 21:53:33 -0500

    Add encrypt/decrypt test

--- test/gpgmex_test.exs
+++ test/gpgmex_test.exs
@@ -2,6 +2,8 @@ defmodule GPGTest do
   use ExUnit.Case
   doctest GPG
 
+  @user_email "matt@silbernagel.dev"
+
   test "gets gpg version" do
     version = GPG.get_engine_version()
     assert version == "1.16.0"
@@ -13,5 +15,15 @@ defmodule GPGTest do
 
   test "gets engine info" do
     assert GPG.get_engine_info().filename == "/usr/bin/gpg"
+  end
+
+  test "encrypt/decrypt" do
+    data = "This data should be encrypted"
+    ctx = GPG.context()
+    cipher = GPG.encrypt_for(ctx, @user_email, data)
+    assert cipher =~ "-----BEGIN PGP MESSAGE-----"
+
+    plain_text = GPG.decrypt_data(ctx, cipher)
+    assert plain_text == data
   end
 end
