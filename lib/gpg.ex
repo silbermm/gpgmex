13e94bc683bfed9e51e6815375588eda9a5ec024
Matt Silbernagel <matt@silbernagel.dev>
2021-12-25 21:21:37 -0500

    Document decrypt

--- lib/gpg.ex
+++ lib/gpg.ex
@@ -19,36 +19,44 @@ defmodule GPG do
   """
 
   @doc "Get the currently installed GPG version."
-  @spec get_engine_version() :: String.t()
+  @spec get_engine_version() :: String.t() | :error
   def get_engine_version do
     version = GPG.NIF.check_version()
     to_string(version)
+  catch
+    _e -> :error
   end
 
   @doc "Get information about the currently installed GPG service."
-  @spec get_engine_info() :: map()
+  @spec get_engine_info() :: map() | :error
   def get_engine_info() do
     ref = GPG.NIF.engine_info()
     filename = GPG.NIF.get_filename(ref)
     %{filename: to_string(filename)}
+  catch
+    _e -> :error
   end
 
   @doc "Creates a context and returns the reference."
-  @spec create_context() :: reference()
+  @spec create_context() :: reference() | :error
   def create_context() do
     # must check_version before creating a context
     # TODO: what if check_version fails?
     _version = GPG.NIF.check_version()
     GPG.NIF.create_context()
+  catch
+    _e -> :error
   end
 
   @doc "Get the public key for an email"
-  @spec get_public_key(binary()) :: binary()
+  @spec get_public_key(binary()) :: binary() | :error
   def get_public_key(email) do
     create_context()
     |> GPG.NIF.public_key(email)
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
+  catch
+    _e -> :error
   end
 
   @doc """
@@ -70,15 +78,24 @@ defmodule GPG do
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
   catch
-    e -> {:error, :keynoexist}
+    _e -> {:error, :keynoexist}
   end
 
-  @doc "Decrypt the given data"
-  @spec decrypt(binary()) :: binary()
+  @doc """
+  Decrypt the given data
+
+  ## Examples
+
+      iex> decrypt("-----BEGIN PGP MESSAGE-----\\n\\nhQIMA1M1Dqrc4va7ARAAtivmEVIg8WAqYgrLBmFbU1iqp2qhfUq9QyPyJLEfmsOg\\nsJm4L7ZG4LKAA9YpEREzmOYr722MfzN2MDy3ssgBJ2/hHBRIXR5fQrlib6dQnyzE\\nkcN2jpUHlmy3p0CTXkH/i1NG3xSRcoapruFvvICCRE+s6zMrtM5qxlEPSV11NHlG\\nEN/wyCfLc66Xu2vGMpLY+9wIeHJmYK7Zpy2K+snHqdnNRnAY2VGZITAcaXjfIjqB\\nKV54ZD1DKebi5P8mJ0pRhgIvCpTVR4+MJk+s5/Rkase6Ckp3jar/Tj5vlbMEPqb7\\nrsp0PoBqE7PNaMXu9sOu/XUwMOLiKsBnpuBojXbrUEHn7/WZ2gd5n1+qax0e9k4X\\nzv+yJ1HV/M6xBsQQfrUB1OoDCHBNjuQPYHcBV7LcQYlBJ80gopgUcsNnZTW9seAN\\nn/6ZUdBeDs7U/CFTinMdOukHp0bqcCd1A69CvCl2zzj/SnNESL01az4wT4AiK3YU\\ntpQ6zznCroxaYd6zJx5xtCBh1xtb4BruRrygvrEI0XpdQ6SU02jr+KqcB3pPhbqI\\nr8woSdHNs2fU+mEGPf2mgPmKAmygnzveE99gpha/dk7NGmnNg3ExQF+jaY4+ADBY\\ndh3Zx9JNurL8EwoNSL/PWw/7suM7vkWy0FaInXVcvEhFfVFu6fRsKPTMJ8+GB9PS\\nSgFHykbYtA3PgISBswfYpI68ynOGRes3jT/Uktu7l4MbDnOere/OAq629awDYG6H\\nFWVc8kcPIRp2LoI8FeYcZz/dj8UJAAP57r58\\n=T/Al\\n-----END PGP MESSAGE-----\\n")
+      "data to encrypt"
+  """
+  @spec decrypt(binary()) :: binary() | :error
   def decrypt(data) do
     create_context()
     |> GPG.NIF.decrypt(data)
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
+  catch
+    _e -> :error
   end
 end
