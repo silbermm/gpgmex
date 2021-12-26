48fe07f13ab3c2b23a87603086f94b5a5be88a06
Matt Silbernagel <silbermm@gmail.com>
2021-12-25 21:23:05 -0500

    Feature/exdoc (#2)
    * Add ex_doc dependency
    * Add docs to all functions
    * move nifs to new module and dont expose docs
    * document the elixir functions
    * add dialyzer
    * Add error result on all functionst
    * Document decrypt

--- lib/gpg.ex
+++ lib/gpg.ex
@@ -15,285 +15,87 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 defmodule GPG do
   @moduledoc """
-  GNU Privacy Guard bindings
+  GnuPG bindings
   """
 
-  use Zig,
-    libs: ["/usr/lib/libgpgme.so"],
-    include: ["/usr/include"],
-    link_libc: true
-
-  ~Z"""
-    const c = @cImport({
-      @cInclude("gpgme.h");
-      @cInclude("locale.h");
-      @cDefine("SIZE", "4092");
-    });
-
-    /// nif: check_version/0
-    fn check_version(env: beam.env) beam.term {
-      var y = c.gpgme_check_version(null);
-      return beam.make_cstring_charlist(env, y);
-    }
-
-    /// nif: check_openpgp_supported/0
-    fn check_openpgp_supported(env: beam.env) bool {
-      var err = c.gpgme_engine_check_version(c.gpgme_protocol_t.GPGME_PROTOCOL_OpenPGP);
-      if (err != c.GPG_ERR_NO_ERROR) {
-          return false;
-      }
-      return true;
-    }
-
-    /// resource: engine_info_struct definition
-    const engine_info_struct = struct {
-      filename: [*c]u8,
-      homedir: [*c]u8,
-    };
-
-    /// resource: context_struct definition
-    const context_struct = struct {
-      context: c.gpgme_ctx_t
-    };
-
-    /// nif: engine_info/0
-    fn engine_info(env: beam.env) beam.term {
-      var enginfo: c.gpgme_engine_info_t = undefined;
-      const err = c.gpgme_get_engine_info(&enginfo);
-
-      if (err != c.GPG_ERR_NO_ERROR) {
-          return 9888;
-      }
-
-      var resource_filename = beam.allocator.alloc(u8, 64)
-        catch return beam.raise_enomem(env);
-      errdefer beam.allocator.free(resource_filename);
-
-      var resource_homedir = beam.allocator.alloc(u8, 64)
-         catch return beam.raise_enomem(env);
-      errdefer beam.allocator.free(resource_homedir);
-
-
-      return __resource__.create(engine_info_struct, env, .{
-          .filename = enginfo.*.file_name,
-          .homedir = enginfo.*.home_dir,
-      }) catch return beam.raise_resource_error(env);
-    }
-
-    /// nif: get_filename/1
-    fn get_filename(env: beam.env, res: beam.term) beam.term {
-      var result = __resource__.fetch(engine_info_struct, env, res)
-          catch return beam.raise_resource_error(env);
-      return beam.make_cstring_charlist(env, result.filename);
-    }
-
-    /// nif: get_homedir/1
-    fn get_homedir(env: beam.env, res: beam.term) beam.term {
-      var result = __resource__.fetch(engine_info_struct, env, res)
-          catch return beam.raise_resource_error(env);
-      return beam.make_cstring_charlist(env, result.homedir);
-    }
-
-    /// nif: create_context/0
-    fn create_context(env: beam.env) beam.term {
-      var ceofcontext: c.gpgme_ctx_t = undefined;
-      var err = c.gpgme_new(&ceofcontext);
-
-      if (err != c.GPG_ERR_NO_ERROR) {
-        return beam.raise_resource_error(env);
-      }
-
-      err = c.gpgme_set_protocol(ceofcontext, c.gpgme_protocol_t.GPGME_PROTOCOL_OpenPGP);
-      
-      if (err != c.GPG_ERR_NO_ERROR) {
-        return beam.raise_resource_error(env);
-      }
-
-      // set engine info in our context
-      err = c.gpgme_ctx_set_engine_info(ceofcontext, c.gpgme_protocol_t.GPGME_PROTOCOL_OpenPGP, "/usr/bin/gpg2", "/home/silbermm/.gnupg/");
-      if (err != c.GPG_ERR_NO_ERROR) {
-        std.log.err("ERROR {}", .{err});
-        return beam.raise_resource_error(env);
-      }
-
-      _ = c.gpgme_set_armor(ceofcontext, 1);
-
-      var resource_context = beam.allocator.alloc(c.gpgme_engine_info_t, 64)
-        catch return beam.raise_enomem(env);
-      errdefer beam.allocator.free(resource_context);
-      
-      return __resource__.create(context_struct, env, .{
-          .context = ceofcontext
-      }) catch return beam.raise_resource_error(env);
-    }
-
-    /// nif: encrypt/3
-    fn encrypt(env: beam.env, res: beam.term, email: beam.term, text: []u8) beam.term {
-      var context = __resource__.fetch(context_struct, env, res)
-          catch return beam.raise_resource_error(env);
-
-      var key: c.gpgme_key_t = undefined;
-      var err = c.gpgme_op_keylist_start(context.context, email, 0);
-      while (err == c.GPG_ERR_NO_ERROR) {
-          err = c.gpgme_op_keylist_next(context.context, &key);
-          if (err != c.GPG_ERR_NO_ERROR) break;
-
-          //var data = "this is a test of data to encrypt";
-
-          var cipher: c.gpgme_data_t = undefined;
-          err = c.gpgme_data_new(&cipher);
-          var to_encrypt: c.gpgme_data_t = undefined;
-
-          err = c.gpgme_data_new_from_mem(&to_encrypt, text.ptr, text.len, 0);
-          var keys = [_]c.gpgme_key_t{ key, null };
-
-          err = c.gpgme_op_encrypt(context.context, &keys, c.gpgme_encrypt_flags_t.GPGME_ENCRYPT_ALWAYS_TRUST, to_encrypt, cipher);
-          if (err != c.GPG_ERR_NO_ERROR) break;
-
-          //_ = c.gpgme_op_encrypt_result(context.context);
-
-          // READ THE ENCRYPTED DATA
-          var d: [c.SIZE]u8 = undefined;
-          var read_bytes = c.gpgme_data_seek(cipher, 0, c.SEEK_SET);
-          var read_new_bytes_2 = c.gpgme_data_read(cipher, &d, c.SIZE);
-          while (read_new_bytes_2 > 0) {
-              read_new_bytes_2 = c.gpgme_data_read(cipher, &d, c.SIZE);
-          }
-
-          // RELEASE THE POINTERS
-          c.gpgme_data_release(to_encrypt);
-          c.gpgme_data_release(cipher);
-          c.gpgme_key_release(key);
-          const buf_slice = d[0..];
-          return beam.make_cstring_charlist(env, buf_slice);
-        }
-        return beam.raise_resource_error(env);
-    }
-
-    /// nif: decrypt/2
-    fn decrypt(env: beam.env, res: beam.term, data: []u8) beam.term {
-      var context = __resource__.fetch(context_struct, env, res)
-          catch return beam.raise_resource_error(env);
-
-      var cipher: c.gpgme_data_t = undefined;
-      var err = c.gpgme_data_new_from_mem(&cipher, data.ptr, data.len, 0);
-
-      var decrypted: c.gpgme_data_t = undefined;
-      err = c.gpgme_data_new(&decrypted);
-      if (err != c.GPG_ERR_NO_ERROR) {
-        std.log.err("unable to build data obj {}", .{err});
-        return beam.raise_resource_error(env);
-      }
-
-      err = c.gpgme_op_decrypt(context.context, cipher, decrypted);
-      if (err != c.GPG_ERR_NO_ERROR) {
-        std.log.err("unable to decrpyt {}", .{err});
-        return beam.raise_resource_error(env);
-      }
-
-
-      // READ THE ENCRYPTED DATA
-      var d: [c.SIZE]u8 = undefined;
-      var read_bytes = c.gpgme_data_seek(decrypted, 0, c.SEEK_SET);
-      var read_new_bytes_2 = c.gpgme_data_read(decrypted, &d, c.SIZE);
-      while (read_new_bytes_2 > 0) {
-        read_new_bytes_2 = c.gpgme_data_read(decrypted, &d, c.SIZE);
-      }
-
-      // RELEASE THE POINTERS
-      c.gpgme_data_release(decrypted);
-      c.gpgme_data_release(cipher);
-      const buf_slice = d[0..];
-      return beam.make_cstring_charlist(env, buf_slice);
-    }
-
-    /// nif: public_key/2
-    fn public_key(env: beam.env, res: beam.term, email: beam.term) beam.term {
-
-      var context = __resource__.fetch(context_struct, env, res)
-          catch return beam.raise_resource_error(env);
-
-      var data: c.gpgme_data_t = undefined;
-      var err = c.gpgme_data_new(&data);
-       
-      if (err != c.GPG_ERR_NO_ERROR) {
-        return beam.raise_resource_error(env);
-      }
-
-      err = c.gpgme_data_set_encoding(data, c.gpgme_data_encoding_t.GPGME_DATA_ENCODING_ARMOR);
-      if (err != c.GPG_ERR_NO_ERROR) {
-        return beam.raise_resource_error(env);
-      }
-
-      // EXPORT AND GET PUBLIC KEY
-      err = c.gpgme_op_export(context.context, email, 0, data);
-      if (err != c.GPG_ERR_NO_ERROR) {
-        return beam.raise_resource_error(env);
-      }
-
-      var read_bytes = c.gpgme_data_seek(data, 0, c.SEEK_END);
-      if (read_bytes == -1) {
-        std.log.err("data-seek-err: {}", .{12});
-        return beam.raise_resource_error(env);
-      }
-
-      read_bytes = c.gpgme_data_seek(data, 0, c.SEEK_SET);
-
-      if (read_bytes == -1) {
-          return beam.raise_resource_error(env);
-      }
-
-      read_bytes = c.gpgme_data_seek(data, 0, c.SEEK_SET);
-
-      var buf: [c.SIZE]u8 = undefined;
-      var read_new_bytes = c.gpgme_data_read(data, &buf, c.SIZE);
-      while (read_new_bytes > 0) {
-          read_new_bytes = c.gpgme_data_read(data, &buf, c.SIZE);
-      }
-      const buf_slice = buf[0..];
-      return beam.make_cstring_charlist(env, buf_slice);
-    }
-  """
-
+  @doc "Get the currently installed GPG version."
+  @spec get_engine_version() :: String.t() | :error
   def get_engine_version do
-    version = GPG.check_version()
+    version = GPG.NIF.check_version()
     to_string(version)
+  catch
+    _e -> :error
   end
 
-  @spec get_engine_info() :: map()
+  @doc "Get information about the currently installed GPG service."
+  @spec get_engine_info() :: map() | :error
   def get_engine_info() do
-    ref = GPG.engine_info()
-    filename = GPG.get_filename(ref)
+    ref = GPG.NIF.engine_info()
+    filename = GPG.NIF.get_filename(ref)
     %{filename: to_string(filename)}
+  catch
+    _e -> :error
   end
 
-  def context() do
+  @doc "Creates a context and returns the reference."
+  @spec create_context() :: reference() | :error
+  def create_context() do
     # must check_version before creating a context
     # TODO: what if check_version fails?
-    _version = GPG.check_version()
+    _version = GPG.NIF.check_version()
+    GPG.NIF.create_context()
+  catch
+    _e -> :error
+  end
+
+  @doc "Get the public key for an email"
+  @spec get_public_key(binary()) :: binary() | :error
+  def get_public_key(email) do
     create_context()
-  end
-
-  @spec get_public_key(reference(), binary()) :: binary()
-  def get_public_key(ctx, email) do
-    ctx
-    |> public_key(email)
+    |> GPG.NIF.public_key(email)
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
+  catch
+    _e -> :error
   end
 
-  def encrypt_for(ctx, email, data) do
-    ctx
-    |> encrypt(email, data)
+  @doc """
+  Encrypt data for the requtested email recipient
+
+  ## Examples
+
+      iex> encrypt("myemail@mydomain.com", "data to encrypt")
+      "-----BEGIN PGP MESSAGE-----\\n\\nhQIMA1M1Dqrc4va7ARAAtivmEVIg8WAqYgrLBmFbU1iqp2qhfUq9QyPyJLEfmsOg\\nsJm4L7ZG4LKAA9YpEREzmOYr722MfzN2MDy3ssgBJ2/hHBRIXR5fQrlib6dQnyzE\\nkcN2jpUHlmy3p0CTXkH/i1NG3xSRcoapruFvvICCRE+s6zMrtM5qxlEPSV11NHlG\\nEN/wyCfLc66Xu2vGMpLY+9wIeHJmYK7Zpy2K+snHqdnNRnAY2VGZITAcaXjfIjqB\\nKV54ZD1DKebi5P8mJ0pRhgIvCpTVR4+MJk+s5/Rkase6Ckp3jar/Tj5vlbMEPqb7\\nrsp0PoBqE7PNaMXu9sOu/XUwMOLiKsBnpuBojXbrUEHn7/WZ2gd5n1+qax0e9k4X\\nzv+yJ1HV/M6xBsQQfrUB1OoDCHBNjuQPYHcBV7LcQYlBJ80gopgUcsNnZTW9seAN\\nn/6ZUdBeDs7U/CFTinMdOukHp0bqcCd1A69CvCl2zzj/SnNESL01az4wT4AiK3YU\\ntpQ6zznCroxaYd6zJx5xtCBh1xtb4BruRrygvrEI0XpdQ6SU02jr+KqcB3pPhbqI\\nr8woSdHNs2fU+mEGPf2mgPmKAmygnzveE99gpha/dk7NGmnNg3ExQF+jaY4+ADBY\\ndh3Zx9JNurL8EwoNSL/PWw/7suM7vkWy0FaInXVcvEhFfVFu6fRsKPTMJ8+GB9PS\\nSgFHykbYtA3PgISBswfYpI68ynOGRes3jT/Uktu7l4MbDnOere/OAq629awDYG6H\\nFWVc8kcPIRp2LoI8FeYcZz/dj8UJAAP57r58\\n=T/Al\\n-----END PGP MESSAGE-----\\n"
+
+      iex> encrypt("someotheremail@non-existant.com", "data to encrypt")
+      {:error, :keynoexist}
+
+  """
+  @spec encrypt(String.t(), binary()) :: binary() | {:error, :keynoexist}
+  def encrypt(email, data) do
+    create_context()
+    |> GPG.NIF.encrypt(email, data)
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
+  catch
+    _e -> {:error, :keynoexist}
   end
 
-  @doc ""
-  def decrypt_data(ctx, data) do
-    ctx
-    |> decrypt(data)
+  @doc """
+  Decrypt the given data
+
+  ## Examples
+
+      iex> decrypt("-----BEGIN PGP MESSAGE-----\\n\\nhQIMA1M1Dqrc4va7ARAAtivmEVIg8WAqYgrLBmFbU1iqp2qhfUq9QyPyJLEfmsOg\\nsJm4L7ZG4LKAA9YpEREzmOYr722MfzN2MDy3ssgBJ2/hHBRIXR5fQrlib6dQnyzE\\nkcN2jpUHlmy3p0CTXkH/i1NG3xSRcoapruFvvICCRE+s6zMrtM5qxlEPSV11NHlG\\nEN/wyCfLc66Xu2vGMpLY+9wIeHJmYK7Zpy2K+snHqdnNRnAY2VGZITAcaXjfIjqB\\nKV54ZD1DKebi5P8mJ0pRhgIvCpTVR4+MJk+s5/Rkase6Ckp3jar/Tj5vlbMEPqb7\\nrsp0PoBqE7PNaMXu9sOu/XUwMOLiKsBnpuBojXbrUEHn7/WZ2gd5n1+qax0e9k4X\\nzv+yJ1HV/M6xBsQQfrUB1OoDCHBNjuQPYHcBV7LcQYlBJ80gopgUcsNnZTW9seAN\\nn/6ZUdBeDs7U/CFTinMdOukHp0bqcCd1A69CvCl2zzj/SnNESL01az4wT4AiK3YU\\ntpQ6zznCroxaYd6zJx5xtCBh1xtb4BruRrygvrEI0XpdQ6SU02jr+KqcB3pPhbqI\\nr8woSdHNs2fU+mEGPf2mgPmKAmygnzveE99gpha/dk7NGmnNg3ExQF+jaY4+ADBY\\ndh3Zx9JNurL8EwoNSL/PWw/7suM7vkWy0FaInXVcvEhFfVFu6fRsKPTMJ8+GB9PS\\nSgFHykbYtA3PgISBswfYpI68ynOGRes3jT/Uktu7l4MbDnOere/OAq629awDYG6H\\nFWVc8kcPIRp2LoI8FeYcZz/dj8UJAAP57r58\\n=T/Al\\n-----END PGP MESSAGE-----\\n")
+      "data to encrypt"
+  """
+  @spec decrypt(binary()) :: binary() | :error
+  def decrypt(data) do
+    create_context()
+    |> GPG.NIF.decrypt(data)
     |> Enum.take_while(&(&1 != 170))
     |> to_string()
+  catch
+    _e -> :error
   end
 end
