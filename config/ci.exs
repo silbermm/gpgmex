8515a145d26494d4fc753443d0b400c93305375b
Matt <matt@silbernagel.dev>
2022-11-07 20:54:30 -0500

    fix tests

--- config/ci.exs
+++ config/ci.exs
@@ -2,4 +2,7 @@ import Config
 
 config :gpgmex,
   include_dir: ["/usr/include/x86_64-linux-gnu", "/usr/include"],
-  libs_dir: ["/usr/lib/x86_64-linux-gnu/libgpgme.so"]
+  libs_dir: ["/usr/lib/x86_64-linux-gnu/libgpgme.so"],
+  gpg_bin: "/usr/bin/gpg",
+  gpg_home: "~/.gnupg",
+  native_api: GPG.MockNativeAPI
