18d5df594d5a8327bbe2d778eca63d17314059c5
Matt <matt@silbernagel.dev>
2023-07-30 00:36:02 -0400

    Update changelog and version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule GPGMEx.MixProject do
   def project do
     [
       app: :gpgmex,
-      version: "0.0.13",
+      version: "0.1.1",
       elixir: "~> 1.12",
       package: package(),
       start_permanent: Mix.env() == :prod,
